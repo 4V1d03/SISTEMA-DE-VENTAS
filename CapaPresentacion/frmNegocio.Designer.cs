2366ee6564dfc816b80b3f70875844cf2289e779
dixiefg04 <119626215+dixiefg04@users.noreply.github.com>
2023-11-20 09:49:17 -0600

    Etapa 13
    Frm Negocio Terminado
    CN_Negocio: funciones para guardar,actualizar y borrar datos
    CD_Negocio : instancia de clase CD_Negocio y validaciones

--- CapaPresentacion/frmNegocio.Designer.cs
+++ CapaPresentacion/frmNegocio.Designer.cs
@@ -31,18 +31,18 @@
             this.label7 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.iconButton2 = new FontAwesome.Sharp.IconButton();
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            this.btnguardarcambios = new FontAwesome.Sharp.IconButton();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.iconButton1 = new FontAwesome.Sharp.IconButton();
+            this.txtdireccion = new System.Windows.Forms.TextBox();
+            this.txtruc = new System.Windows.Forms.TextBox();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.btnSubir = new FontAwesome.Sharp.IconButton();
             this.label2 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
             this.SuspendLayout();
             // 
             // label7
@@ -51,9 +51,10 @@
             this.label7.BackColor = System.Drawing.Color.Honeydew;
             this.label7.Font = new System.Drawing.Font("Agency FB", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.Teal;
-            this.label7.Location = new System.Drawing.Point(187, 63);
+            this.label7.Location = new System.Drawing.Point(249, 78);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(196, 31);
+            this.label7.Size = new System.Drawing.Size(237, 36);
             this.label7.TabIndex = 23;
             this.label7.Text = "Descripción del Negocio";
             // 
@@ -66,66 +67,74 @@
             this.label1.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.DarkSlateGray;
             this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(554, 485);
+            this.label1.Size = new System.Drawing.Size(738, 597);
             this.label1.TabIndex = 22;
             // 
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.Color.MediumAquamarine;
-            this.groupBox1.Controls.Add(this.pictureBox1);
-            this.groupBox1.Controls.Add(this.iconButton2);
+            this.groupBox1.Controls.Add(this.picLogo);
+            this.groupBox1.Controls.Add(this.btnguardarcambios);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.textBox3);
-            this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Controls.Add(this.iconButton1);
+            this.groupBox1.Controls.Add(this.txtdireccion);
+            this.groupBox1.Controls.Add(this.txtruc);
+            this.groupBox1.Controls.Add(this.txtnombre);
+            this.groupBox1.Controls.Add(this.btnSubir);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Location = new System.Drawing.Point(1, 109);
+            this.groupBox1.Location = new System.Drawing.Point(1, 134);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(552, 288);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(736, 354);
             this.groupBox1.TabIndex = 24;
             this.groupBox1.TabStop = false;
             // 
-            // pictureBox1
+            // picLogo
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.White;
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(35, 83);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(143, 112);
-            this.pictureBox1.TabIndex = 10;
-            this.pictureBox1.TabStop = false;
+            this.picLogo.BackColor = System.Drawing.Color.White;
+            this.picLogo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picLogo.Location = new System.Drawing.Point(47, 102);
+            this.picLogo.Margin = new System.Windows.Forms.Padding(4);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(190, 137);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picLogo.TabIndex = 10;
+            this.picLogo.TabStop = false;
             // 
-            // iconButton2
+            // btnguardarcambios
             // 
-            this.iconButton2.BackColor = System.Drawing.Color.LightCyan;
-            this.iconButton2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.iconButton2.Font = new System.Drawing.Font("Agency FB", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.iconButton2.ForeColor = System.Drawing.Color.DarkGreen;
-            this.iconButton2.IconChar = FontAwesome.Sharp.IconChar.CloudUploadAlt;
-            this.iconButton2.IconColor = System.Drawing.Color.DarkGreen;
-            this.iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.iconButton2.IconSize = 30;
-            this.iconButton2.Location = new System.Drawing.Point(249, 207);
-            this.iconButton2.Name = "iconButton2";
-            this.iconButton2.Size = new System.Drawing.Size(194, 31);
-            this.iconButton2.TabIndex = 9;
-            this.iconButton2.Text = "Guardar Cambios";
-            this.iconButton2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.iconButton2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.iconButton2.UseVisualStyleBackColor = false;
+            this.btnguardarcambios.BackColor = System.Drawing.Color.LightCyan;
+            this.btnguardarcambios.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnguardarcambios.Font = new System.Drawing.Font("Agency FB", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnguardarcambios.ForeColor = System.Drawing.Color.DarkGreen;
+            this.btnguardarcambios.IconChar = FontAwesome.Sharp.IconChar.CloudUploadAlt;
+            this.btnguardarcambios.IconColor = System.Drawing.Color.DarkGreen;
+            this.btnguardarcambios.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnguardarcambios.IconSize = 30;
+            this.btnguardarcambios.Location = new System.Drawing.Point(332, 255);
+            this.btnguardarcambios.Margin = new System.Windows.Forms.Padding(4);
+            this.btnguardarcambios.Name = "btnguardarcambios";
+            this.btnguardarcambios.Size = new System.Drawing.Size(259, 38);
+            this.btnguardarcambios.TabIndex = 9;
+            this.btnguardarcambios.Text = "Guardar Cambios";
+            this.btnguardarcambios.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnguardarcambios.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnguardarcambios.UseVisualStyleBackColor = false;
+            this.btnguardarcambios.Click += new System.EventHandler(this.btnguardarcambios_Click);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Honeydew;
-            this.label5.Location = new System.Drawing.Point(192, 150);
+            this.label5.Location = new System.Drawing.Point(256, 185);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(77, 19);
+            this.label5.Size = new System.Drawing.Size(96, 24);
             this.label5.TabIndex = 8;
             this.label5.Text = "Dirección:";
             // 
@@ -134,9 +143,10 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Honeydew;
-            this.label4.Location = new System.Drawing.Point(191, 94);
+            this.label4.Location = new System.Drawing.Point(255, 116);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(51, 19);
+            this.label4.Size = new System.Drawing.Size(61, 24);
             this.label4.TabIndex = 7;
             this.label4.Text = "R.U.C:";
             // 
@@ -145,78 +155,87 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Honeydew;
-            this.label3.Location = new System.Drawing.Point(192, 35);
+            this.label3.Location = new System.Drawing.Point(256, 43);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(153, 19);
+            this.label3.Size = new System.Drawing.Size(189, 24);
             this.label3.TabIndex = 6;
             this.label3.Text = "Nombre del Negocio:";
             // 
-            // textBox3
+            // txtdireccion
             // 
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(193, 173);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(308, 22);
-            this.textBox3.TabIndex = 5;
+            this.txtdireccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtdireccion.Location = new System.Drawing.Point(257, 213);
+            this.txtdireccion.Margin = new System.Windows.Forms.Padding(4);
+            this.txtdireccion.Name = "txtdireccion";
+            this.txtdireccion.Size = new System.Drawing.Size(409, 26);
+            this.txtdireccion.TabIndex = 5;
             // 
-            // textBox2
+            // txtruc
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(193, 118);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(308, 22);
-            this.textBox2.TabIndex = 4;
+            this.txtruc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtruc.Location = new System.Drawing.Point(257, 145);
+            this.txtruc.Margin = new System.Windows.Forms.Padding(4);
+            this.txtruc.Name = "txtruc";
+            this.txtruc.Size = new System.Drawing.Size(409, 26);
+            this.txtruc.TabIndex = 4;
             // 
-            // textBox1
+            // txtnombre
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(193, 60);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(308, 22);
-            this.textBox1.TabIndex = 3;
+            this.txtnombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnombre.Location = new System.Drawing.Point(257, 74);
+            this.txtnombre.Margin = new System.Windows.Forms.Padding(4);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(409, 26);
+            this.txtnombre.TabIndex = 3;
             // 
-            // iconButton1
+            // btnSubir
             // 
-            this.iconButton1.BackColor = System.Drawing.Color.LightCyan;
-            this.iconButton1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.iconButton1.Font = new System.Drawing.Font("Agency FB", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.iconButton1.ForeColor = System.Drawing.Color.RoyalBlue;
-            this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.CloudUploadAlt;
-            this.iconButton1.IconColor = System.Drawing.Color.RoyalBlue;
-            this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.iconButton1.IconSize = 30;
-            this.iconButton1.Location = new System.Drawing.Point(55, 205);
-            this.iconButton1.Name = "iconButton1";
-            this.iconButton1.Size = new System.Drawing.Size(104, 36);
-            this.iconButton1.TabIndex = 2;
-            this.iconButton1.Text = "Subir";
-            this.iconButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.iconButton1.UseVisualStyleBackColor = false;
+            this.btnSubir.BackColor = System.Drawing.Color.LightCyan;
+            this.btnSubir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSubir.Font = new System.Drawing.Font("Agency FB", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubir.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.btnSubir.IconChar = FontAwesome.Sharp.IconChar.CloudUploadAlt;
+            this.btnSubir.IconColor = System.Drawing.Color.RoyalBlue;
+            this.btnSubir.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnSubir.IconSize = 30;
+            this.btnSubir.Location = new System.Drawing.Point(73, 252);
+            this.btnSubir.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSubir.Name = "btnSubir";
+            this.btnSubir.Size = new System.Drawing.Size(139, 44);
+            this.btnSubir.TabIndex = 2;
+            this.btnSubir.Text = "Subir";
+            this.btnSubir.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSubir.UseVisualStyleBackColor = false;
+            this.btnSubir.Click += new System.EventHandler(this.btnSubir_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Honeydew;
-            this.label2.Location = new System.Drawing.Point(36, 57);
+            this.label2.Location = new System.Drawing.Point(48, 70);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 23);
+            this.label2.Size = new System.Drawing.Size(70, 29);
             this.label2.TabIndex = 1;
             this.label2.Text = "Logo:";
             // 
             // frmNegocio
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(844, 485);
+            this.ClientSize = new System.Drawing.Size(1125, 597);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmNegocio";
             this.Text = "frmNegocio";
+            this.Load += new System.EventHandler(this.frmNegocio_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -227,15 +246,15 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private FontAwesome.Sharp.IconButton iconButton1;
+        private FontAwesome.Sharp.IconButton btnSubir;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private FontAwesome.Sharp.IconButton iconButton2;
+        private System.Windows.Forms.TextBox txtdireccion;
+        private System.Windows.Forms.TextBox txtruc;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.PictureBox picLogo;
+        private FontAwesome.Sharp.IconButton btnguardarcambios;
     }
 }
